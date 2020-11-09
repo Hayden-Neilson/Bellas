a70d6f92c3e1f3b929c96102e8f11f78f9849161
Hayden-Neilson <haydenneilson5@gmail.com>
2020-11-08 19:39:18 -0700

    fixed bug in build

--- amplifyapp/src/index.js
+++ amplifyapp/src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
-Amplify.configure(config);
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+Amplify.configure(config);
+
 
 ReactDOM.render(
   <React.StrictMode>
